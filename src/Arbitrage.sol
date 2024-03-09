816071bfca658524f8c58cd5215b58020ceabbc2
OnlyF0uR <29165327+OnlyF0uR@users.noreply.github.com>
2024-03-09 11:48:08 +0100

    finalisation

--- src/Arbitrage.sol
+++ src/Arbitrage.sol
@@ -6,21 +6,23 @@ import "@balancer-labs/v2-interfaces/contracts/vault/IFlashLoanRecipient.sol";
 import "@balancer-labs/v2-interfaces/contracts/solidity-utils/openzeppelin/IERC20.sol";
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract Arbitrage is IFlashLoanRecipient  {
   IVault private immutable vault;
   address private immutable minter;
 
-  struct SwapInfo {
-    address poolAddress;
-    address tokenIn;
-    uint24 poolFee;
-    bool isLegacy;
-  }
-  struct ArbitInfo {
-    SwapInfo buy;
-    SwapInfo sell;
+  struct Info {
+    address tokenToBorrow;
+
+    address poolOneRouter;
+    address poolTwoRouter;
+
+    address mediatorToken;
+    uint24 poolOneFee;
+    uint24 poolTwoFee;
+    uint256 poolOneAmountOutMin;
+    uint256 poolTwoAmountOutMin;
   }
 
   constructor(address _flashAddrProvider) {
@@ -33,8 +35,14 @@ contract Arbitrage is IFlashLoanRecipient  {
     _;
   }
 
-  function execute(ArbitInfo memory _meta, uint256 _amount) external {
-    // ...
+  function execute(Info memory _meta, uint256 _amount) external {
+    IERC20[] memory tokens = new IERC20[](1);
+    tokens[0] = IERC20(_meta.tokenToBorrow);
+    uint256[] memory amounts = new uint256[](1);
+    amounts[0] = _amount;
+
+    bytes memory userData = abi.encode(_meta);
+    vault.flashLoan(this, tokens, amounts, userData);
   }
 
   function collect(address _token, address _receiver) external minterOnly {
@@ -43,19 +51,15 @@ contract Arbitrage is IFlashLoanRecipient  {
 
     IERC20(_token).transfer(_receiver, bal);
   }
-
-  function receiveFlashLoan(IERC20[] memory tokens, uint256[] memory amounts, uint256[] memory feeAmounts, bytes memory userData) external override {
-    // ...
-  }
-  
-  ISwapRouter constant v3Router = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
   
   function performV3Swap(
-      address tokenIn,
-      address tokenOut,
-      uint24 poolFee,
-      uint256 amountIn
-  ) external returns (uint256 amountOut) {
+    address _router,
+    address tokenIn,
+    address tokenOut,
+    uint24 poolFee,
+    uint256 amountIn
+  ) public returns (uint256 amountOut) {
+    ISwapRouter v3Router = ISwapRouter(_router);
     IERC20(tokenIn).approve(address(v3Router), amountIn);
 
     ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
@@ -72,9 +76,14 @@ contract Arbitrage is IFlashLoanRecipient  {
     amountOut = v3Router.exactInputSingle(params);
   }
 
-  IUniswapV2Router private v2Router = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-
-  function performV2Swap(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOutMin) external returns (uint256 amountOut) {
+  function performV2Swap(
+    address _router,
+    address tokenIn, 
+    address tokenOut, 
+    uint256 amountIn, 
+    uint256 amountOutMin
+  ) public returns (uint256 amountOut) {
+    IUniswapV2Router02 v2Router = IUniswapV2Router02(_router);
     IERC20(tokenIn).approve(address(v2Router), amountIn);
 
     address[] memory path;
@@ -87,5 +96,32 @@ contract Arbitrage is IFlashLoanRecipient  {
     );
 
     return amounts[1];
+  }
+
+  function receiveFlashLoan(IERC20[] memory tokens, uint256[] memory amounts, uint256[] memory feeAmounts, bytes memory userData) external override {
+    require(msg.sender == address(vault), "MC");
+
+    Info memory meta = abi.decode(userData, (Info));
+    // Did we get the correct token?
+    require(address(tokens[0]) == meta.tokenToBorrow, "TNE");
+
+    uint256 midAmount;
+    
+    // Buy
+    if (meta.poolOneAmountOutMin == 0) {
+      midAmount = performV3Swap(meta.poolOneRouter, meta.tokenToBorrow, meta.mediatorToken, meta.poolOneFee, amounts[0]);
+    } else {
+      midAmount = performV2Swap(meta.poolOneRouter, meta.tokenToBorrow, meta.mediatorToken, amounts[0], meta.poolOneAmountOutMin);
+    }
+
+    // Sell
+    if (meta.poolTwoAmountOutMin == 0) {
+      performV3Swap(meta.poolTwoRouter, meta.mediatorToken, meta.tokenToBorrow, meta.poolTwoFee, midAmount);
+    } else {
+      performV2Swap(meta.poolTwoRouter, meta.mediatorToken, meta.tokenToBorrow, midAmount, meta.poolTwoAmountOutMin);
+    }
+
+    uint256 totalDebt = amounts[0] + feeAmounts[0];
+    IERC20(meta.tokenToBorrow).transfer(address(vault), totalDebt);
   }
 }
--- test/Arbitrage.t.sol
+++ test/Arbitrage.t.sol
@@ -9,8 +9,7 @@ address constant BALANCER_VAULT = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
 
 address constant WETH = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
 address constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-
-address constant WETH_MATIC_POOL = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
+address constant LINK = 0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39;
 
 contract ArbitrageTest is Test {
     Arbitrage private arbit;
@@ -23,23 +22,47 @@ contract ArbitrageTest is Test {
         to: address(arbit),
         give: 100e18
       });
-
-      console2.log("WETH balance before swaps", IERC20(WETH).balanceOf(address(arbit)));
     }
 
     function testV3Swap() public {
-      uint256 result = arbit.performV3Swap(WETH, WMATIC, 500, 1e18);
+      console2.log("WMATIC balance before v3 swap", IERC20(WMATIC).balanceOf(address(arbit)));
+      uint256 result = arbit.performV3Swap(0xE592427A0AEce92De3Edee1F18E0157C05861564, WETH, WMATIC, 500, 1e18);
       console2.log("WMATIC balance after v3 swap", result);
 
       assertGt(IERC20(WMATIC).balanceOf(address(arbit)), 0);
     }
 
     function testV2Swap() public {
-      uint256 before = IERC20(WMATIC).balanceOf(address(arbit));
+      console2.log("LINK balance before v2 swap", IERC20(LINK).balanceOf(address(arbit)));
+      uint256 result = arbit.performV2Swap(0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff, WETH, LINK, 1e18, 1);
+      console2.log("LINK balance after v2 swap", result);
 
-      uint256 result = arbit.performV2Swap(WETH, WMATIC, 1e18, 1);
-      console2.log("WMATIC balance after v2 swap", result);
+      assertGt(IERC20(LINK).balanceOf(address(arbit)), 0);
+    }
 
-      assertGt(IERC20(WMATIC).balanceOf(address(arbit)), before);
+    function testFlashLoan() public {
+      uint256 balanceBefore = IERC20(WETH).balanceOf(address(arbit));
+      console2.log("WEHT balance before flash loan", balanceBefore);
+
+      Arbitrage.Info memory _meta;
+      _meta.tokenToBorrow = WETH;
+      _meta.poolOneRouter = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+      _meta.poolTwoRouter = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
+
+      _meta.mediatorToken = WMATIC;
+      _meta.poolOneFee = 500;
+      _meta.poolTwoFee = 0;
+
+      _meta.poolOneAmountOutMin = 0;
+      _meta.poolTwoAmountOutMin = 1;
+
+      arbit.execute(_meta, 5e17);
+
+      uint256 balanceAfter = IERC20(WETH).balanceOf(address(arbit));
+      console2.log("WEHT balance after flash loan", IERC20(WETH).balanceOf(address(arbit)));
+
+      console2.log("WETH diff (0.00)", balanceBefore - balanceAfter);
+
+      assertNotEq(balanceBefore, balanceAfter); // Something must be lost or gained during the trades
     }
 }
